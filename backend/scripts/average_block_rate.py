8a592acd9fbb654101e9e8a06c719985763c884b
prashundey <60023261+prashundey@users.noreply.github.com>
2021-12-26 16:24:59 -0500

    Script to Check Average Mine Rate. Hexidecimal Hashes to easy for CPU

---
+++ backend/scripts/average_block_rate.py
@@ -0,0 +1,28 @@
+import time
+
+from backend.blockchain.blockchain import Blockchain
+from backend.config import SECONDS
+
+blockchain = Blockchain()
+times = []
+
+for i in range(1000):
+    start_time = time.time_ns()
+    blockchain.add_block(i)
+    end_time = time.time_ns()
+    mining_time = (end_time - start_time) / SECONDS
+    
+    times.append(mining_time)
+    average_time = sum(times) / len(times)
+
+    print(f'Lastest Difficulty: {blockchain.chain[-1].difficulty}')
+    print(f'Lastest Mine Rate: {mining_time}s')
+    print(f'AVERAGE MINE RATE: {average_time} \n')
+
+'''
+HexiDecimal Form Hashes:
+    Average Mine Rate Converges to 2.5 seconds but MINE_RATE = 4 seconds
+
+    TODO: change hashes to Binary Representation, because mining leading zeros for 
+    Hexidecimal hashes to easy to for CPU
+'''
