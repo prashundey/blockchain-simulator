d040f07fa300a3c7b177a2b70081425fb04b782b
prashundey <60023261+prashundey@users.noreply.github.com>
2021-12-28 18:02:46 -0500

    PubSub class and initial methods created -- working on setting up peer instances

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -1,46 +1,9 @@
-# import time
-# from pubnub.pubnub import PubNub 
-# from pubnub.pnconfiguration import PNConfiguration
-# from pubnub.callbacks import SubscribeCallback
-
-# class Listener(SubscribeCallback):
-#     """
-#     """
-#     def message(self, pubnub, message):
-#         print(f'\n-- incoming message: {message}')
-#         #return super().message(pubnub, message)
-
-
-
-# pnconfig = PNConfiguration()
-# pnconfig.subscribe_key = 'sub-c-9f0c2442-6789-11ec-8481-8a57b2c2a271'
-# pnconfig.publish_key = 'pub-c-594a87f2-c3a7-4069-9264-e35c9431b70f'
-
-# pubnub = PubNub(pnconfig)
-
-# pubnub.add_listener(Listener())
-
-# TEST_CHANNEL = 'TEST_CHANNEL'
-# pubnub.subscribe().channels([TEST_CHANNEL]).execute()
-
-
-# def my_publish_callback(envelope, status):
-#     print(envelope)
-#     print(envelope, status)
-
-# def main():
-#     time.sleep(1)
-#     pubnub.publish().channel([TEST_CHANNEL]).message({'foo': 'bar'}).pn_async(my_publish_callback)
-    
-
-# if __name__ == "__main__":
-#     main()
-
-from pubnub.callbacks import SubscribeCallback
-from pubnub.enums import PNStatusCategory
+import pprint
+import time
+from pubnub.pubnub import PubNub 
 from pubnub.pnconfiguration import PNConfiguration
-from pubnub.pubnub import PubNub
-from pprint import pprint
+from pubnub.callbacks import SubscribeCallback
+
 
 class Listener(SubscribeCallback):
     def status(self, pubnub, status):
@@ -50,24 +13,36 @@ class Listener(SubscribeCallback):
         pprint(presence.__dict__)
 
     def message(self, pubnub, message):
-        pprint(message.__dict__)
-
-def my_publish_callback(envelope, status):
-    print(envelope, status)
+        print(f'incoming message -- {message.channel} | {message.message}')
 
 pnconfig = PNConfiguration()
-pnconfig.subscribe_key = "sub-c-9f0c2442-6789-11ec-8481-8a57b2c2a271"
-pnconfig.publish_key = "pub-c-594a87f2-c3a7-4069-9264-e35c9431b70f"
+pnconfig.subscribe_key = 'sub-c-9f0c2442-6789-11ec-8481-8a57b2c2a271'
+pnconfig.publish_key = 'pub-c-594a87f2-c3a7-4069-9264-e35c9431b70f'
+TEST_CHANNEL = 'TEST_CHANNEL'
 
-pubnub = PubNub(pnconfig)
-pubnub.add_listener(Listener())
 
-pubnub.subscribe()\
-    .channels("Test-Channel")\
-    .with_presence()\
-    .execute()\
+class PubSub():
+    """
+    Handles functionality of publish/subcribe layer of application.
+    Porvides communication between all the nodes on the blockchain network
+    """
 
-pubnub.publish()\
-    .channel("Test-Channel")\
-    .message({'foo' : 'bar'})\
-    .pn_async(my_publish_callback)
+    def __init__(self) -> None:
+        self.pubnub = PubNub(pnconfig)
+        self.pubnub.subscribe().channels([TEST_CHANNEL]).execute()
+        self.pubnub.add_listener(Listener())
+
+    def publish(self, channel, message):
+        """
+        Publish the message object to the channel
+        """
+        self.pubnub.publish().channel(channel).message(message).sync()
+
+def main():
+    pubsub = PubSub()
+    time.sleep(1)
+    pubsub.publish(TEST_CHANNEL, {'foo': 'bar'})
+
+if __name__ == '__main__':
+    main()
+
