2f155d22f5085f035ba10fbcbced96152d26df53
prashundey <60023261+prashundey@users.noreply.github.com>
2021-12-28 21:48:42 -0500

    Wallet data verification using public key. sha256 hash algorithm used for implementation. Special methods provided by cryptography lib

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -5,7 +5,9 @@ import json
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePublicKey
 from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidSignature
 
 class Wallet:
     """
@@ -19,7 +21,7 @@ class Wallet:
         self.private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
         self.public_key = self.private_key.public_key()
 
-    def sign(self, data):
+    def sign(self, data : any) -> bytes:
         """
         Generate signature based on the data using local Private Key
         Stringify and Encode input data utf-8
@@ -29,6 +31,23 @@ class Wallet:
             ec.ECDSA(hashes.SHA256())
         )
 
+    @staticmethod
+    def verify(public_key : EllipticCurvePublicKey, 
+                data : any, 
+                signature : bytes) -> bool:
+        """
+        Verify signature based on the orginal public key and data input (needs encoding)
+        """ 
+        try:
+            public_key.verify(
+                signature, 
+                json.dumps(data).encode('utf-8'), 
+                ec.ECDSA(hashes.SHA256())
+            )
+            return True
+        except InvalidSignature:
+            return False
+
 
 def main():
     wallet = Wallet()
@@ -37,6 +56,12 @@ def main():
     data = {'foo' : 'bar'}
     signature = wallet.sign(data)
     print(f'signature: {signature}')
+
+    is_valid = Wallet.verify(wallet.public_key, data, signature)
+    print(f'is valid: {is_valid}')
+
+    is_not_valid = Wallet.verify(Wallet().public_key, data, signature)
+    print(f'is not valid: {is_not_valid}')
 
 if __name__ == '__main__':
     main()
